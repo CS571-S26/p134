965b08206debbec12885e3002dc7f8de7eecb552
Myron Neal <myronneal@Myrons-MacBook-Air.local>
2026-03-18 23:36:07 -0500

    fixed website display

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/p134",
   plugins: [react()],
-  base: "/p134"
+  build: {
+    outDir: "docs"
+  }
 })
